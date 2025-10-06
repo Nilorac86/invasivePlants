f65ee2fa5dd8ecb62174b486b0cce954f861d7cc
carolineriksson <carolin86@live.se>
2025-10-06 11:35:01 +0200

    Added dependencys

---
+++ src/main/java/com/carolin/invasiveplants/InvasivePlantsApplication.java
@@ -0,0 +1,13 @@
+package com.carolin.invasiveplants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvasivePlantsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvasivePlantsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carolin/invasiveplants/InvasivePlantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carolin.invasiveplants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvasivePlantsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
