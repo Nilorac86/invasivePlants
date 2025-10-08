c64c1b3b2bb9be1daa67d735eb4f1bd9bd7c6e3e
Filip <anders.hallberg74@gmail.com>
2025-10-08 12:25:04 +0200

    #31 Entites, not done problem in Plant

---
+++ src/main/java/com/carolin/invasiveplants/Entity/Role.java
@@ -0,0 +1,49 @@
+package com.carolin.invasiveplants.Entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    @Column(length = 45)
+    private String name;
+
+    @Column(length = 45)
+    private String admin;
+
+    // Constructors
+    public Role() {
+    }
+
+    // Getters and Setters
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(String admin) {
+        this.admin = admin;
+    }
+
+
+
+}
---
+++ src/main/java/com/carolin/invasiveplants/Entity/UserReward.java
@@ -0,0 +1,71 @@
+package com.carolin.invasiveplants.Entity;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "user_rewards")
+public class UserReward {
+
+    @EmbeddedId
+    private UserRewardId id;
+
+    @ManyToOne
+    @MapsId("userId")  // maps userId part of the embedded key
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @MapsId("rewardId")  // maps rewardId part of the embedded key
+    @JoinColumn(name = "reward_id")
+    private Reward reward;
+
+    @Column(name = "awarded_at")
+    private LocalDateTime awardedAt;
+
+    // Constructors
+    public UserReward() {
+    }
+
+    public UserReward(User user, Reward reward) {
+        this.user = user;
+        this.reward = reward;
+        this.id = new UserRewardId(user.getUserId(), reward.getRewardId());
+        this.awardedAt = LocalDateTime.now();
+    }
+
+    // Getters and Setters
+    public UserRewardId getId() {
+        return id;
+    }
+
+    public void setId(UserRewardId id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Reward getReward() {
+        return reward;
+    }
+
+    public void setReward(Reward reward) {
+        this.reward = reward;
+    }
+
+    public LocalDateTime getAwardedAt() {
+        return awardedAt;
+    }
+
+    public void setAwardedAt(LocalDateTime awardedAt) {
+        this.awardedAt = awardedAt;
+    }
+}
