027dc7e7e6a81c6e46b6e08f8b1a19727956c2b3
carolineriksson <carolin86@live.se>
2025-10-15 13:10:34 +0200

    Added a security config class

---
+++ src/main/java/com/carolin/invasiveplants/SecurityConfig/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.carolin.invasiveplants.SecurityConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+
+public class SecurityConfig  {
+
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
+
+        http
+
+                .csrf(csrf -> csrf.disable())
+
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/plants/info").permitAll());
+
+                return http.build();
+
+    }
+}
