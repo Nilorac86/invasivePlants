580e2fdf4ace1ab60f3d67f391f0be776a59182f
carolineriksson <carolin86@live.se>
2025-10-16 11:50:51 +0200

    added react to project again after fail

--- src/main/java/com/carolin/invasiveplants/Controller/SpeciesController.java
+++ src/main/java/com/carolin/invasiveplants/Controller/SpeciesController.java
@@ -3,12 +3,14 @@ package com.carolin.invasiveplants.Controller;
 import com.carolin.invasiveplants.ResponseDTO.SpeciesBasicInfoResponse;
 import com.carolin.invasiveplants.Service.SpeciesService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/plants")
 @RestController
 public class SpeciesController {
