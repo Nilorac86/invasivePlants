83917822712307199d1d8bde98eacd17691ecd45
carolineriksson <carolin86@live.se>
2025-10-27 17:09:39 +0100

    Put in path for profile and changed the order in routes.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PlantCardsPage from "./Pages/PlantCardsPage";
 import LoginPage from "./Pages/LoginPage";
 import Header from "./components/Header";
+import ProfilePage from "./Pages/ProfilePage";
+
 
 function App() {
-  const [User, setUser] = useState(null);
+  
+  const [user, setUser] = useState(null);
 
   const handleLoginSuccess = (userData) => {
     setUser(userData);
@@ -14,13 +17,12 @@ function App() {
 
   return (
     <Router>
-      <Header user={User} />
+      <Header user={user} />
       <Routes>
-        <Route
-          path="/login"
-          element={<LoginPage onLoginSuccess={handleLoginSuccess} />}
+        <Route path ="/" element={<PlantCardsPage />} />
+        <Route path="/login" element={<LoginPage onLoginSuccess={handleLoginSuccess} />}
         />
-        <Route path="/" element={<PlantCardsPage />} />
+        <Route path ="/profile" element={<ProfilePage userId={user?.id}/>} />
       </Routes>
     </Router>
   );
