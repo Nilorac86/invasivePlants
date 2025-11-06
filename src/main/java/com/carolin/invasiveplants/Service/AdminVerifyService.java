d05168faf5004753c9ca390b75531b40f5ddf138
goddessLeona <petra.j-son@outlook.com>
2025-11-06 20:32:08 +0100

    Created a service

---
+++ src/main/java/com/carolin/invasiveplants/Service/AdminVerifyService.java
@@ -0,0 +1,33 @@
+package com.carolin.invasiveplants.Service;
+
+import com.carolin.invasiveplants.Entity.Plant;
+import com.carolin.invasiveplants.Enum.PlantStatus;
+import com.carolin.invasiveplants.Repository.PlantRepository;
+import com.carolin.invasiveplants.RequestDTO.AdminVerifyRequestDTO;
+import jakarta.annotation.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class AdminVerifyService {
+
+    private final PlantRepository plantRepository;
+
+    public AdminVerifyService(PlantRepository plantRepository) {
+        this.plantRepository = plantRepository;
+    }
+
+    //service update the old status to a new status on the reported plant
+    public void updateReportedPlantsStatus(Long id, PlantStatus newStatus){
+
+        Plant reportedPlant = plantRepository.findById(id)
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND, "Reported plant not found"));
+
+        PlantStatus oldStatus = reportedPlant.getStatus();
+        reportedPlant.setStatus(newStatus);
+
+        plantRepository.save(reportedPlant);
+    }
+
+}
