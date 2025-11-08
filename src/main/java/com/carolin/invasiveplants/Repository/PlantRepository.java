e141f998e12407f733d0d0578d2032f866d6c1b8
goddessLeona <petra.j-son@outlook.com>
2025-11-08 21:01:08 +0100

    added a special query to make sure it finds the right user, the one that made Removed_by_user

--- src/main/java/com/carolin/invasiveplants/Repository/PlantRepository.java
+++ src/main/java/com/carolin/invasiveplants/Repository/PlantRepository.java
@@ -2,8 +2,16 @@ package com.carolin.invasiveplants.Repository;
 
 import com.carolin.invasiveplants.Entity.Plant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface PlantRepository extends JpaRepository<Plant, Long> {
+
+    @Query("SELECT p FROM Plant p LEFT JOIN FETCH p.removedBy WHERE p.plantId = :id")
+    Optional<Plant> findByIdWithRemovedBy(@Param("id")Long id);
+
 }
