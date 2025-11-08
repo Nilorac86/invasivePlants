d65ebbcadca6a0de651df89bf0b3fd78113a6d6d
goddessLeona <petra.j-son@outlook.com>
2025-11-08 20:09:04 +0100

    added notification in service where admin verify or deline the posts with status REMOVED

--- src/main/java/com/carolin/invasiveplants/Service/AdminVerifyService.java
+++ src/main/java/com/carolin/invasiveplants/Service/AdminVerifyService.java
@@ -1,21 +1,30 @@
 package com.carolin.invasiveplants.Service;
 
+import com.carolin.invasiveplants.Entity.Notification;
 import com.carolin.invasiveplants.Entity.Plant;
+import com.carolin.invasiveplants.Enum.NotificationType;
 import com.carolin.invasiveplants.Enum.PlantStatus;
+import com.carolin.invasiveplants.Repository.NotificationRepository;
 import com.carolin.invasiveplants.Repository.PlantRepository;
 import com.carolin.invasiveplants.RequestDTO.AdminVerifyRequestDTO;
 import jakarta.annotation.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDateTime;
+
 @Service
+@Transactional
 public class AdminVerifyService {
 
     private final PlantRepository plantRepository;
+    private final NotificationRepository notificationRepository;
 
-    public AdminVerifyService(PlantRepository plantRepository) {
+    public AdminVerifyService(PlantRepository plantRepository, NotificationRepository notificationRepository) {
         this.plantRepository = plantRepository;
+        this.notificationRepository = notificationRepository;
     }
 
     //service update the old status to a new status on the removed plant
@@ -32,6 +41,22 @@ public class AdminVerifyService {
 
         removedPlant.setStatus(newStatus);
         plantRepository.save(removedPlant);
+
+        //Create and save a notification for the user
+        Notification notification = new Notification();
+        notification.setUser(removedPlant.getRemovedBy());
+        notification.setTime(LocalDateTime.now());
+
+        //sending the user that removed plant a message depending on admin, approve or decline
+        if (newStatus == PlantStatus.VERIFIED){
+            notification.setMessage("Your removed plant has been verified! You have earned points");
+            notification.setNotificationType(NotificationType.SUCCESS);
+        } else if (newStatus == PlantStatus.REGISTERED) {
+            notification.setMessage("Your removed plant report was declined.");
+            notification.setNotificationType(NotificationType.WARNING);
+        }
+
+        notificationRepository.save(notification);
     }
 
 }
