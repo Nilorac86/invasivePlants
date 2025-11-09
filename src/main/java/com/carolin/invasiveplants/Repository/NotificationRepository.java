f001d09c35ba8e00ffe8e5abf8a80aaa755963e7
goddessLeona <petra.j-son@outlook.com>
2025-11-09 15:00:47 +0100

    added some stuff in the notification repository

--- src/main/java/com/carolin/invasiveplants/Repository/NotificationRepository.java
+++ src/main/java/com/carolin/invasiveplants/Repository/NotificationRepository.java
@@ -2,10 +2,22 @@ package com.carolin.invasiveplants.Repository;
 
 
 import com.carolin.invasiveplants.Entity.Notification;
+import com.carolin.invasiveplants.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    List<Notification> findByUser(User user);
+
+    @Query("SELECT n FROM Notification n WHERE n.user.userId = :userId")
+    List<Notification> findByUserId(@Param("userId") Long userId);
+
+    List<Notification> findByUserAndReadFalse(User user);
 
 }
