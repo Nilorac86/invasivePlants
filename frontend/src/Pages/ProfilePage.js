0e79deee8b43d0b5949f1fffcba8450b0ef6f45f
goddessLeona <petra.j-son@outlook.com>
2025-11-11 20:40:55 +0100

    added the notification conection on the profilepage

--- frontend/src/Pages/ProfilePage.js
+++ frontend/src/Pages/ProfilePage.js
@@ -1,33 +1,50 @@
 import {useEffect, useState} from "react";
 import { fetchUserProfile } from "../service/ProfileService";
+import { fetchUserNotifications, fetchUserNotifications } from "../service/NotificationService";
 import ProfileInfo from "../components/Profile";
+import NotificationsList from "../components/Notifications";
 
 function ProfilePage() {
   const [profile, setProfile] = useState(null);
+  const [notifications, setNotifications] =useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-  
 
-    async function loadProfile() {
-      try {
+    async function loadUserData(){
+
+      try{
+        // Fetch profile first
         const data = await fetchUserProfile();
         setProfile(data);
-      } catch (err) {
-        setError("Could not fetch userdata");
-      }
-    }
 
-    loadProfile();
+        //fetch notifications for the loged in user,if any
+        const userNotifications = await fetchUserNotifications();
+        setNotifications(userNotifications);
+
+      }catch(err){
+        console.error("Error fetshing userdata or notifications:", err);
+        setError("could not fetch user data or notifications")
+      }
+
+    }
+     loadUserData();
   }, []);
 
-  // If the user cant be fetched it will show error
+  // If the user cant be fetched it will show error or loading state
   if (error) return <p>{error}</p>;
   if (!profile) return <p>Laddar....</p>; 
 
-  // User data will get fetched but needs to be more details when the profile gets done.
-  return <ProfileInfo data={profile} />;
+  return(
+    //The <div> is just a wrapper element. React components must return a single JSX element. 
+    // So if you have multiple components or elements to render, you need to wrap them in a container like a
+    //  <div> or a React Fragment (<>...</>).
+    <div>
+      <ProfileInfo data={profile} />
+      <NotificationsList notifications = {notifications} />
+    </div>
+  );
 }
-
-export default ProfilePage;
+  export default ProfilePage;
+  
 
