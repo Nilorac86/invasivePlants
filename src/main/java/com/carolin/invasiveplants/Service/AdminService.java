e9913d21bcb96a5c71188db21b5eef3eb49f83dc
goddessLeona <petra.j-son@outlook.com>
2025-11-13 15:33:04 +0100

    changed service to work with the new table

--- src/main/java/com/carolin/invasiveplants/Service/AdminService.java
+++ src/main/java/com/carolin/invasiveplants/Service/AdminService.java
@@ -2,10 +2,13 @@ package com.carolin.invasiveplants.Service;
 
 import com.carolin.invasiveplants.Entity.Notification;
 import com.carolin.invasiveplants.Entity.Plant;
+import com.carolin.invasiveplants.Entity.RemovedPlant;
+import com.carolin.invasiveplants.Entity.User;
 import com.carolin.invasiveplants.Enum.NotificationType;
 import com.carolin.invasiveplants.Enum.PlantStatus;
 import com.carolin.invasiveplants.Repository.NotificationRepository;
 import com.carolin.invasiveplants.Repository.PlantRepository;
+import com.carolin.invasiveplants.Repository.RemovePlantRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,10 +22,12 @@ public class AdminService {
 
     private final PlantRepository plantRepository;
     private final NotificationRepository notificationRepository;
+    private final RemovePlantRepository removePlantRepository;
 
-    public AdminService(PlantRepository plantRepository, NotificationRepository notificationRepository) {
+    public AdminService(PlantRepository plantRepository, NotificationRepository notificationRepository, RemovePlantRepository removePlantRepository) {
         this.plantRepository = plantRepository;
         this.notificationRepository = notificationRepository;
+        this.removePlantRepository = removePlantRepository;
     }
 
 
@@ -31,8 +36,14 @@ public class AdminService {
     //service update the old status to a new status on the removed plant
     public void updateReportedPlantsStatus(Long id, PlantStatus newStatus){
 
-        Plant removedPlant = plantRepository.findByIdWithRemovedBy(id)
-                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND, "Removed plant not found"));
+        //Find the reported plant by ID, throw 404 if not found
+        Plant reportedPlant = plantRepository.findById(id)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Reported plant not found"));
+
+        //Find the removed plant associated with the reported plant
+        RemovedPlant removedPlant = removePlantRepository.findByReportedPlantId(id)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "removed plant record not found"));
+
 
         //double check so it really is reported as REMOVED before changing status
         if(removedPlant.getStatus() != PlantStatus.REMOVED) {
@@ -41,11 +52,14 @@ public class AdminService {
         }
 
         removedPlant.setStatus(newStatus);
-        plantRepository.save(removedPlant);
+        removePlantRepository.save(removedPlant);
 
-        //Create and save a notification for the user
+        //Notification for user who removed the plant
+        User removedByUser= removedPlant.getRemovedBy();
+
         Notification notification = new Notification();
-        notification.setUser(removedPlant.getRemovedBy());
+
+        notification.setUser(removedByUser);
         notification.setTime(LocalDateTime.now());
 
         //sending the user that removed plant a message depending on admin, approve or decline
