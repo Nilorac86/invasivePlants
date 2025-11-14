8686a2b50de23443878844f65e24214e8c9d4f0f
goddessLeona <petra.j-son@outlook.com>
2025-11-14 11:12:55 +0100

    changed ListRemovedPlantsMapper and ListRemovedPlantsResponseDTO

--- src/main/java/com/carolin/invasiveplants/Mapper/ListRemovedPlantsMapper.java
+++ src/main/java/com/carolin/invasiveplants/Mapper/ListRemovedPlantsMapper.java
@@ -2,9 +2,11 @@ package com.carolin.invasiveplants.Mapper;
 
 
 import com.carolin.invasiveplants.Entity.Plant;
+import com.carolin.invasiveplants.Entity.RemovedPlant;
 import com.carolin.invasiveplants.ResponseDTO.ListRemovedPlantsResponseDTO;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,6 +14,41 @@ import java.util.stream.Collectors;
 @Component
 public class ListRemovedPlantsMapper {
 
+    public ListRemovedPlantsResponseDTO toDTO (RemovedPlant removedPlant){
+
+        if (removedPlant == null) return null;
+
+        String userName = "unknown";
+
+        // Check so that user is not null
+        if(removedPlant.getRemovedBy() !=null){
+            String firstName = removedPlant.getRemovedBy().getFirstName() != null
+                    ? removedPlant.getRemovedBy().getFirstName()
+                    : "";
+            String lastName = removedPlant.getRemovedBy().getLastName() != null
+                    ? removedPlant.getRemovedBy().getLastName()
+                    : "";
+
+            userName = (firstName + " " + lastName).trim();
+        }
+
+        return new ListRemovedPlantsResponseDTO(
+                removedPlant.getReportedPlant().getSpecies().getSpeciesName(),
+                userName,
+                removedPlant.getRemovedAt(),
+                removedPlant.getPhotoAfter());
+
+    }
+
+    public List<ListRemovedPlantsResponseDTO> toDto(List<RemovedPlant> removedPlants) {
+        if (removedPlants == null || removedPlants.isEmpty()) return Collections.emptyList();
+
+        return removedPlants.stream()
+                .map(this::toDTO)
+                .collect(Collectors.toList());
+
+    }
+    /*
     public ListRemovedPlantsResponseDTO toDTO (Plant plant){
         if (plant == null) return null;
 
@@ -44,5 +81,5 @@ public class ListRemovedPlantsMapper {
                 .map(this::toDTO)
                 .collect(Collectors.toList());
 
-    }
+    }*/
 }
