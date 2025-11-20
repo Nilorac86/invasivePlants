d3be19e7de61b485f1c6dae426108ae2b07989e6
carolineriksson <carolin86@live.se>
2025-11-20 13:52:25 +0100

    Added user controller

---
+++ src/main/java/com/carolin/invasiveplants/Controller/UserController.java
@@ -0,0 +1,13 @@
+package com.carolin.invasiveplants.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/user")
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class UserController {
+
+
+}
