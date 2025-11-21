5ece5e427a4f83c1358e4ea0dc940db96ce300a5
goddessLeona <petra.j-son@outlook.com>
2025-11-21 20:03:12 +0100

    added styling

--- frontend/src/components/DashboardUser.jsx
+++ frontend/src/components/DashboardUser.jsx
@@ -11,23 +11,43 @@ function Dashboard({data}){
     return (
       <div className="form-container">
         <div className="points">
-          <h3>Dina poäng</h3>
+          <h3 id="h3">Dina poäng</h3>
           <p>{data.points}</p>
         </div>
 
         <div className="pending">
-          <h3>Borttagna växter - väntar på godkännande </h3>
-          <p>{data.pendingTotal}</p>
+          <h3 id="h3">
+            Borttagna växter - väntar på godkännande ({data.pendingTotal}){" "}
+          </h3>
+          {data.pendingPreview.map((plant) => (
+            <div key={plant.id}>
+              {plant.plantName} -{" "}
+              {new Date(plant.removedAt).toLocaleDateString()}
+            </div>
+          ))}
         </div>
 
         <div className="approved">
-          <h3>Borttagna växter - godkända</h3>
-          <p>{data.approvedTotal}</p>
+          <h3 id="h3">Borttagna växter - godkända({data.approvedTotal}) </h3>
+          {data.approvedPreview.map((plant) => (
+            <div key={plant.id}>
+              {plant.plantName} -{" "}
+              {new Date(plant.removedAt).toLocaleDateString()}
+            </div>
+          ))}
         </div>
 
         <div className="gift">
-          <h3>Mottagna gåvor </h3>
-          <p>{data.rewardsTotal}</p>
+          <h3 id="h3">Mottagna gåvor ({data.giftsTotal})</h3>
+          {data.giftsPreview && data.giftsPreview.length > 0 ? (
+            data.giftsPreview.map((reward) => (
+              <div key={reward.rewardId} className="reward-item">
+                <strong>{reward.rewardTitle}</strong> — {reward.points} poäng
+              </div>
+            ))
+          ) : (
+            <p>Inga gåvor än</p>
+          )}
         </div>
       </div>
     ); 
