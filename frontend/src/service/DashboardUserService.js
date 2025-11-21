5d6497b17e31c70d8bb8e41a09499cae4104d7ba
goddessLeona <petra.j-son@outlook.com>
2025-11-21 17:09:46 +0100

    Created a DashboardUserService

---
+++ frontend/src/service/DashboardUserService.js
@@ -0,0 +1,17 @@
+//DashboardUserService.js
+
+import { apiGet } from "../util/Api";
+
+// Fetching the information from backend
+
+export async function getdashboard() {
+  try {
+    return await apiGet("/user/dashboard", {
+      method: "GET",
+      credentials: "include",
+    });
+  } catch (error) {
+    console.error("Error fetching the dashbord list:", error);
+    throw error;
+  }
+}
