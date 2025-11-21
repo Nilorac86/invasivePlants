d47e82d5a1547964590553c23575063a77295b1b
carolineriksson <carolin86@live.se>
2025-11-21 13:10:47 +0100

    Added method register user

--- src/main/java/com/carolin/invasiveplants/Service/UserService.java
+++ src/main/java/com/carolin/invasiveplants/Service/UserService.java
@@ -1,8 +1,45 @@
 package com.carolin.invasiveplants.Service;
 
+import com.carolin.invasiveplants.Entity.User;
+import com.carolin.invasiveplants.ExceptionHandler.ApiException;
+import com.carolin.invasiveplants.Mapper.UserRegisterMapper;
+import com.carolin.invasiveplants.Repository.UserRepository;
+import com.carolin.invasiveplants.RequestDTO.UserRegisterRequestDto;
+import com.carolin.invasiveplants.ResponseDTO.UserRegisterResponseDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserRegisterMapper userRegisterMapper;
+
+    public UserService(UserRepository userRepository, UserRegisterMapper userRegisterMapper) {
+        this.userRepository = userRepository;
+        this.userRegisterMapper = userRegisterMapper;
+    }
+
+
+    //#################################### REGISTER A USER ##################################################
+
+    public UserRegisterResponseDto createUser(UserRegisterRequestDto userRegisterRequestDto) {
+
+            // Check if email already exist.
+        if (userRepository.findByEmail(userRegisterRequestDto.getEmail()).isPresent()) {
+            throw new ApiException("E-mail finns redan", HttpStatus.CONFLICT);
+        }
+
+            // Map to entity and then save in user.
+            User user = userRegisterMapper.toEntity(userRegisterRequestDto);
+
+            User savedUser = userRepository.save(user);
+
+            // Converts entity to responseDto
+            UserRegisterResponseDto responseDto = userRegisterMapper.toDto(savedUser);
+
+            return responseDto;
+
+    }
 
 }
