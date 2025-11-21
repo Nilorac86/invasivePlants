84d82f529f00cc44b641107c27983323d8b7d462
carolineriksson <carolin86@live.se>
2025-11-21 13:10:22 +0100

    Added responseDto.

--- src/main/java/com/carolin/invasiveplants/ResponseDTO/UserRegisterResponseDto.java
+++ src/main/java/com/carolin/invasiveplants/ResponseDTO/UserRegisterResponseDto.java
@@ -2,4 +2,44 @@ package com.carolin.invasiveplants.ResponseDTO;
 
 public class UserRegisterResponseDto {
 
+    private String fullName;
+
+    private String email;
+
+    private String phoneNumber;
+
+    public UserRegisterResponseDto() {
+    }
+
+    public UserRegisterResponseDto(String fullName, String email, String phoneNumber) {
+        this.fullName = fullName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String firstName) {
+        this.fullName = fullName;
+    }
+
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
