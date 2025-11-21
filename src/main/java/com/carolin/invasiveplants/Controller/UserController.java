a1eb0c29e663d208065791746cade6b524ada7a1
goddessLeona <petra.j-son@outlook.com>
2025-11-21 12:58:04 +0100

    added new user controller

---
+++ src/main/java/com/carolin/invasiveplants/Controller/UserController.java
@@ -0,0 +1,41 @@
+package com.carolin.invasiveplants.Controller;
+
+import com.carolin.invasiveplants.Entity.User;
+import com.carolin.invasiveplants.ResponseDTO.UserProfileDashboardResponseDto;
+import com.carolin.invasiveplants.Service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/dashboard")
+    public ResponseEntity<UserProfileDashboardResponseDto> getUserProfileDashboard(
+            @AuthenticationPrincipal User user){
+
+        if (user == null) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+        // pass userId and points seperatly
+        UserProfileDashboardResponseDto dto = userService.getUserProfileDashboard(user.getUserId(),3);
+
+        // if service did not fetch points set them hear;
+        dto.setPoints(user.getPoints());
+
+        return ResponseEntity.ok(dto);
+    }
+
+
+}
